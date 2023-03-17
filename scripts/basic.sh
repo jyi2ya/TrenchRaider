eac5db7a35131cefdeeb78524043357f7937834e
jyi2ya <jyi2ya@outlook.com>
2023-03-17 19:10:49 +0800

    test: 自动化测试！

---
+++ scripts/basic.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -eu
+IFS=$(printf "\n\t")
+# scratch=$(mktemp -d -t tmp.XXXXXXXXXX)
+# atexit() {
+#   rm -rf "$scratch"
+# }
+# trap atexit EXIT
+
+cd "$(dirname "$0")"
+if ! command -v oidc-test-client >/dev/null; then
+    echo 快去下载 oidc tester！就在 https://github.com/BeryJu/oidc-test-client
+    exit 1
+fi
+
+if ! lsof -i:3000 >/dev/null; then
+    echo oidc 服务还没跑起来，快去跑
+    exit 1
+fi
+
+export OIDC_BIND=http://127.0.0.1:3000
+export OIDC_CLIENT_ID=23333
+export OIDC_CLIENT_SECRET=Ther3IsN0Exc3pt1onInThi5Libr4ry #for i know i'll always go with you~
+export OIDC_ROOT_URL="http://127.0.0.1:3000/auth"
+export OIDC_SCOPES="openid,email,profile"
+export OIDC_DO_REFRESH=true
+export OIDC_DO_INTROSPECTION=true
+export OIDC_DO_USER_INFO=true
+export OIDC_TLS_VERYFI=false
+
+oidc-test-client
