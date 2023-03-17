070fea2290217dfeebee9c85363b75ba490da82f
jyi2ya <jyi2ya@outlook.com>
2023-03-17 11:20:05 +0800

    fix: 客户端获取 token 改为 POST 接口

--- src/main.pl
+++ src/main.pl
@@ -293,7 +293,7 @@ get '/oauth/confirm_authorize' => sub {
 
 # 然后第三方 app 再用 code 在这里拿 token。
 # 妈的，这个还要支持 refresh_token
-get '/oauth/token' => sub {
+post '/oauth/token' => sub {
     my ($c) = @_;
 
     $c->expect(
